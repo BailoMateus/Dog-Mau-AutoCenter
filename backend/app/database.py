e0e7b28895a480d8c4fa4c1f4b1def81bdd0ba94
Mateus Bailo <mateus_bailo@hotmail.com>
2026-03-19 22:00:50 -0300

    Fix: pls

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,4 +1,5 @@
 import os
+import urllib.parse
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -10,8 +11,10 @@ DB_NAME = os.getenv("DB_NAME")
 INSTANCE_CONNECTION_NAME = os.getenv("INSTANCE_CONNECTION_NAME")
 
 
+safe_password = urllib.parse.quote_plus(DB_PASS) if DB_PASS else ""
+
 SQLALCHEMY_DATABASE_URL = (
-    f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@/{DB_NAME}?"
+    f"postgresql+psycopg2://{DB_USER}:{safe_password}@/{DB_NAME}?"
     f"host=/cloudsql/{INSTANCE_CONNECTION_NAME}"
 )
 
