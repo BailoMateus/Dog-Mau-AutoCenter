1fac8a3311b53ed6d7888ed0f04846b41ed27ef7
NunesIcaro <icaronunes999@gmail.com>
2026-04-13 21:48:55 -0300

    fix: Arrumando o nome do componente do carrossel

--- frontend/componentes-js/carrossel.js
+++ frontend/componentes-js/carrossel.js
@@ -1,5 +1,5 @@
 // Ativar o data-bs-ride="carousel" caso queira que haja o autoplay
-const carrossel = `
+export const carrossel = `
     <div id="myCarousel" class="carousel slide carousel-fade" data-bs-ride="false">
         <div class="carousel-indicators">
           <button
@@ -26,17 +26,17 @@ const carrossel = `
         <div class="carousel-inner">
           <div class="carousel-item active">
             <div class="container">
-              <img src="" class="d-block w-100" alt="Slide 1" id="carrosselItem1">
+              <img class="d-block w-100" alt="Slide 1" id="carrosselItem1">
             </div>
           </div>
           <div class="carousel-item">
             <div class="container">
-              <img src="" class="d-block w-100" alt="Slide 2" id="carrosselItem2">
+              <img class="d-block w-100" alt="Slide 2" id="carrosselItem2">
             </div>
           </div>
           <div class="carousel-item">
             <div class="container">
-              <img src="" class="d-block w-100" alt="Slide 3" id="carrosselItem3">
+              <img class="d-block w-100" alt="Slide 3" id="carrosselItem3">
             </div>
           </div>
         </div>
@@ -60,6 +60,3 @@ const carrossel = `
         </button>
     </div>
 `;
-
-
-document.getElementById("carrossel").innerHTML = carrossel;
