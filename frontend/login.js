ff2d68aa5a6fa77329243001c422e1642151abe2
Mateus Bailo <mateus_bailo@hotmail.com>
2026-04-16 19:36:59 -0300

    Fix: Login with google

--- frontend/login.js
+++ frontend/login.js
@@ -1,3 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBLY-5JOkHA6YP4viYw5SFB3H1VmoKXzNo",
+    authDomain: "dog-mau-autocenter.firebaseapp.com",
+    projectId: "dog-mau-autocenter",
+    storageBucket: "dog-mau-autocenter.firebasestorage.app",
+    messagingSenderId: "210021860949",
+    appId: "1:210021860949:web:e7ce12df0e9adad49536dc",
+    measurementId: "G-HMRJC0BJXL"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.getElementById("loginForm");
     
@@ -54,6 +71,34 @@ document.addEventListener("DOMContentLoaded", () => {
                 // Erro de rede ou indisponibilidade
                 console.error("Erro na requisição /auth/login:", error);
                 alert("Erro ao tentar conectar com o servidor. Tente novamente mais tarde.");
+            }
+        });
+    }
+
+    const btnGoogleLogin = document.getElementById("btnGoogleLogin");
+    if (btnGoogleLogin) {
+        btnGoogleLogin.addEventListener("click", async () => {
+            try {
+                const result = await signInWithPopup(auth, provider);
+                const user = result.user;
+                const idToken = await user.getIdToken();
+                console.log("Firebase SignIn Realizado. Token:", idToken);
+
+                const response = await fetch(`${window.BASE_URL}/auth/google`, {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({ id_token: idToken })
+                });
+
+                const data = await response.json();
+                if (!response.ok) throw new Error(data.detail || "Erro ao conectar com servidor");
+
+                alert("Acesso Google efetuado com sucesso!");
+                localStorage.setItem("access_token", data.access_token);
+                window.location.href = "index.html";
+            } catch (error) {
+                console.error("Erro no fluxo do Google Login", error);
+                alert("Falha no login social. Verifique o console.");
             }
         });
     }
