9b18d7a03b4e89338b0e5ff0126687666ebe1900
NunesIcaro <icaronunes999@gmail.com>
2026-06-11 20:02:34 -0300

    liveserver

--- backend/app/main.py
+++ backend/app/main.py
@@ -61,7 +61,12 @@ logger.info("StaticFiles /uploads -> %s", get_uploads_root())
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5500",
+        "http://127.0.0.1:5500",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
